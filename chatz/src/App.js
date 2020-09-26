1f9335b19d273c904d53b8eb839134484c749b69
Christian Rojas <cjorjas72@gmail.com>
2020-09-26 17:50:20 -0400

    firebase add

--- chatz/src/App.js
+++ chatz/src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// firebase dependencies 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD_YU0qnrrd8VLctSW9GyxQsagq-zX-6dQ",
+  authDomain: "chatz-b7f42.firebaseapp.com",
+  databaseURL: "https://chatz-b7f42.firebaseio.com",
+  projectId: "chatz-b7f42",
+  storageBucket: "chatz-b7f42.appspot.com",
+  messagingSenderId: "213497369078",
+  appId: "1:213497369078:web:4cfa5e44c858b12a4077d7",
+  measurementId: "G-W2FFRYFCZE"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
